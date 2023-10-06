cd40ef3717f6c2322b8dc8bb5c945657bce1a3c1
hvsk004 <himanth2004@gmail.com>
2023-10-06 10:29:11 +0530

    Commit

---
+++ Java-Script/objects.js
@@ -0,0 +1,14 @@
+let newObj = {
+  user: "Himanth",
+  email: "himanth2004@gmail.com",
+  Branch: "B.Tech CSE",
+};
+// console.log(newObj);
+// console.log(newObj.Branch);
+// console.log(newObj.hasOwnProperty("section"));
+let obj1 = new Object();
+obj1 = {
+  room: 709,
+  block: "41A",
+};
+console.log(obj1);
